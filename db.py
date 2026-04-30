243da88a3ef1709320817d9c2de97a21d1b75aa0
Annie <ens-rafaelsobrinho@ugv.edu.br>
2026-04-29 21:21:42 -0300

    FEAT: Adicionado configuração de conexão com o banco

--- db.py
+++ db.py
@@ -1,1 +1,11 @@
-.
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from datetime import datetime
+
+DB_CONFIG = {
+    "host": "localhost",
+    "port": 5432,
+    "database": "shellmate",
+    "user": "postgres",
+    "password": "postgres"
+}
